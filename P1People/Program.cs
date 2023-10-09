8c8479b76b5009230927861217d7cf5b100a0b3a
augustlewen <augustlewen@live.com>
2023-10-09 13:58:56 +0200

    Completed P1 and P1_1

---
+++ P1People/Program.cs
@@ -0,0 +1,11 @@
+﻿Person[] people = new Person[3]{new Person(), new Person(), new Person()};
+
+foreach (Person person in people)
+{
+    Console.WriteLine(person);
+}
+
+public class Person
+{
+    
+}
