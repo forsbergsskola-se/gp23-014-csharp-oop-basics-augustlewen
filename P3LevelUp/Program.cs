f7646e1ef9a664c092eec8db7b9f5b686a90e01d
augustlewen <augustlewen@live.com>
2023-10-09 15:01:13 +0200

    Completed P3

---
+++ P3LevelUp/Program.cs
@@ -0,0 +1,44 @@
+﻿Player player = new Player();
+
+while (true)
+{
+    Console.WriteLine($"{player.level} Level and {player.experience} Experience");
+    player.experience += GetInputValue();
+    LevelUpCheck();
+}
+
+void LevelUpCheck()
+{
+    if (player.experience >= 100)
+    {
+        player.level++;
+        player.experience -= 100;
+        LevelUpCheck();
+    }
+}
+
+int GetInputValue()
+{
+    int value = 0;
+    while (value == 0)
+    {
+        string input = Console.ReadLine();
+        if (!int.TryParse(input, out value))
+            value = 0;
+
+        if (value <= 0)
+            value = 0;
+
+    }
+
+    return value;
+}
+
+
+
+
+public class Player
+{
+    public int experience;
+    public int level;
+}
